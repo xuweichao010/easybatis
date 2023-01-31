5c45db03aec74988fb379c17512087aaff7dd27a
xuweichao <xuweichao010@163.com>
2023-01-31 11:03:52 +0800

    feat between测试完成

--- easy-mybatis-common/src/test/java/com/xwc/open/easybatis/mysql/generator/SimpleSelectSourceGeneratorTest.java
+++ easy-mybatis-common/src/test/java/com/xwc/open/easybatis/mysql/generator/SimpleSelectSourceGeneratorTest.java
@@ -114,7 +114,7 @@ public class SimpleSelectSourceGeneratorTest {
         Method method = Reflection.chooseMethod(interfaceClass, methodName);
         OperateMethodMeta operateMethodMeta = easyBatisConfiguration.getOperateMethodAssistant()
                 .getOperateMethodMeta(interfaceClass, method);
-        String expected = "<script> SELECT `id`,`org_code`,`org_name`,`name`,`data_type`,`age`,`job`,`create_time`,`create_id`,`create_name`,`update_time`,`update_id`,`update_name`,`valid` FROM t_user WHERE 1 = 1 AND age BETWEEN #{age} AND #{ageTo} </script>";
+        String expected = "<script> SELECT `id`,`org_code`,`org_name`,`name`,`data_type`,`age`,`job`,`create_time`,`create_id`,`create_name`,`update_time`,`update_id`,`update_name`,`valid` FROM t_user <where>  <if test='age != null and ageTo != null'> AND age BETWEEN #{age} AND #{ageTo} </if> </where> </script>";
         Assert.assertEquals(expected, sourceGenerator.select(operateMethodMeta));
     }
 
